ed3b320636164915423a26d0344c15fe1fab2dd3
Pirate-Emperor <piratekingrahul@gmail.com>
2024-03-17 17:35:00 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='AetherMind',
+    version='0.1',
+    packages=find_packages(),
+    url='https://github.com/Pirate-Emperor/AetherMind',
+    license='Pirate-Emperor',
+    author='Pirate-Emperor',
+    author_email='piratekingrahul.com',
+    description='AetherMind is an intelligent LLM-powered agent project designed to explore the potential and applications of agents based on Large Language Models (LLMs). Built using Streamlit and LangChain, this project provides tools and utilities for creating and managing advanced LLM-based workflows.',
+    install_requires=[],
+)
